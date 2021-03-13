8cf564723810cc94b3d9c6588a588293d46dab27
Daeho Lee <67696504+leedaeho1188@users.noreply.github.com>
2021-03-13 17:54:30 +0900

    9184

--- test.py
+++ test.py
@@ -590,11 +590,37 @@
 #         break
 #     six_n += 1
 
+# --------------------------9184번--------------------------------
+# from sys import stdin
+# read = stdin.readline
+# memo = {
 
+# }
 
+# def w(a, b, c):
+#   if (a, b, c) in memo:
+#     return memo[(a, b, c)]
 
+#   elif a <= 0 or b <= 0 or c <= 0:
+#     return 1
+  
+#   elif a > 20 or b > 20 or c > 20:
+#     return w(20, 20, 20)
 
+#   elif a < b and b < c:
+#     w_num = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+#     memo[(a, b, c)] = w_num
+#     return w_num
+  
+#   else: 
+#     w_num = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+#     memo[(a, b, c)] = w_num
+#     return w_num
+# lst = []
+# while True:
+#   a, b, c = map(int, read().split())
+#   if a == -1 and b == -1 and c == -1 :
+#     break
+#   print('w(%d, %d, %d) = %d' % (a, b, c, w(a, b, c)))
 
-
-
-
+# -------------------------------9461------------------------------------------
