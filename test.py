42fb74b3bc487e9c66124139b85260b22b95f454
Daeho Lee <67696504+leedaeho1188@users.noreply.github.com>
2021-03-15 22:38:52 +0900

    1010

--- test.py
+++ test.py
@@ -812,6 +812,24 @@
 #   num_d *= x
 # print(int((num_r/num_k)/num_d))
 
+# ------------------------------1010------------------------------
+from sys import stdin
+read = stdin.readline
+for _ in range(int(read())):
+  N, M = map(int, read().split())
+  num_n = 1
+  num_m = 1
+  num_d = 1
+  for i in range(1, N+1):
+    num_n *= i
+  for j in range(1, M+1):
+    num_m *= j
+  for x in range(1, M-N+1):
+    num_d *= x
+  print(int((num_m//num_n)//num_d))
+
+
+
 
 
 
