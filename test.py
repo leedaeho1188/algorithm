945eddc6582241467475c2aaca75a4e055d8b29b
Daeho Lee <67696504+leedaeho1188@users.noreply.github.com>
2021-03-16 20:48:43 +0900

    1260

--- test.py
+++ test.py
@@ -813,7 +813,7 @@
 # print(int((num_r/num_k)/num_d))
 
 # ------------------------------1010------------------------------
-# # 조합을 이용해서 문제를 풀어야한다.
+# 조합을 이용해서 문제를 풀어야한다.
 # from sys import stdin
 # read = stdin.readline
 # for _ in range(int(read())):
@@ -859,7 +859,52 @@
 #   lst[i] += lst[i-1]
 # print(sum(lst))
 
-# ----------------------------------------------------------
+# ------------------------------1260----------------------------
+# from sys import stdin
+# read = stdin.readline
+# N, M, V = map(int, read().split())
+# graph = [[0]*(N+1) for _ in range(N+1)]
+# visited = [False]*(N+1)
+
+# for _ in range(M):
+#   x, y = map(int, read().split())
+#   graph[x][y] = 1
+#   graph[y][x] = 1
+
+# def dfs(V):
+#   visited[V] = True
+#   print(V, end=" ")
+#   for i in range(1, N+1):
+#     if not visited[i] and graph[V][i] == 1:
+#       dfs(i)
+
+# def bfs(V):
+#   visited[V] = False
+#   queue = [V]
+#   while queue:
+#     V = queue.pop(0)
+#     print(V, end=" ")
+#     for i in range(1, N+1):
+#       if visited[i] and graph[V][i] == 1:
+#         queue.append(i)
+#         visited[i] = False
+
+
+# dfs(V)
+# print()
+# bfs(V)
+
+# -----------------------------1260--------------------------------
+from sys import stdin
+read = stdin.readline
+lst = []
+N = int(read())
+for _ in range(N):
+  lst.append(int(read()))
+print(round((sum(lst))/N))
+lst.sort()
+if N%2 == 0:
+  print()
 
 
 
