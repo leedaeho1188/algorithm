ac8c48f3c143be268eee9986a2f44bcbf7e9e2d1
DaehoLee <leedaeho1188@gmail.com>
2023-10-31 21:22:20 +0900

    :sparkles: programmers-js-level2-1차프렌즈4블록

---
+++ Programmers/js/level2/[1차] 프렌즈4블록.js
@@ -0,0 +1,16 @@
+function solution(m, n, board) {
+  var answer = 0;
+
+  let prevRow = {};
+  let nextRow = {};
+
+  board.forEach((row) => {
+    for(let i = 0; i < row.length; i++){
+      
+    }
+  })
+
+  return answer;
+}
+
+const theAnswer = solution(4,	5,	["CCBDE", "AAADE", "AAABF", "CCBBF"])
