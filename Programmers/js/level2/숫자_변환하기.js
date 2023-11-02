4d5f2b3429d9c6bfabe752b150338535026a036a
DaehoLee <leedaeho1188@gmail.com>
2023-11-02 13:53:13 +0900

    :sparkles: programmers-js-level2-숫자변환하기(progress)

---
+++ Programmers/js/level2/숫자_변환하기.js
@@ -0,0 +1,21 @@
+function solution(x, y, n) {
+  var answer = 0;
+  const division = y/x;
+  const remainder = y%x;
+
+  const answerArray = [];
+
+  //2로 곱하거나 3으로 곱하거나 둘다 딱 안나눠 떨어지는 경우
+  if(division%2 !== 0 && division%3 !== 0){
+    while(true){
+      
+    }
+  }
+
+
+  return answer;
+}
+
+const theAnswer = solution(10, 40, 30);
+
+console.log(theAnswer);
